5c7a16a74237e8d2d71829cdb5c06b62cfe0ce0d
join0life <158464763+join0life@users.noreply.github.com>
2025-08-13 14:35:51 +0900

    [level 1] Title: 평균 구하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12944. 평균 구하기/평균 구하기.js
@@ -0,0 +1,12 @@
+function solution(arr) {
+    return arr.reduce((acc, cur) => acc + cur) / arr.length;
+    
+//     let result = 0;
+    
+//     for(let v of arr) {
+//         result += v;
+//     }
+    
+//     return result / arr.length;
+    
+}
