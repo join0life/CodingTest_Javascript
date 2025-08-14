941234ba094daba40442b16c48eed469f5b6f3f7
join0life <158464763+join0life@users.noreply.github.com>
2025-08-14 11:23:54 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,9 +1,9 @@
 function solution(price) {
     if (price >= 500000) {
         price = Math.floor(price * 0.80);
-    } else if (price >= 300000 && price < 500000) {
+    } else if (price >= 300000) {
         price = Math.floor(price * 0.90);
-    } else if (price >= 100000 && price < 300000) {
+    } else if (price >= 100000) {
         price = Math.floor(price * 0.95);
     }
     
