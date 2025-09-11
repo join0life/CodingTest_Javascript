5f0b7a08de34553dd9435410ca276c0c02bf271d
join0life <158464763+join0life@users.noreply.github.com>
2025-09-11 10:53:48 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -0,0 +1,11 @@
+function solution(my_string) {
+    let result = ''
+    
+    for(let str of my_string) {
+        if (!result.includes(str)) {
+            result += str;
+        }
+    }
+    
+    return result;
+}
