90ab2e957b5774fe399ebd3f4f5b99ed83628189
join0life <158464763+join0life@users.noreply.github.com>
2025-09-15 10:19:32 +0900

    [level 0] Title: 약수 구하기, Time: 0.98 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
@@ -1,9 +1,10 @@
 function solution(n) {
-    let result = [];
+//     let result = [];
     
-    for(let i = 1; i <= n; i++) {
-        if (n % i === 0) result.push(i);
-    }
+//     for(let i = 1; i <= n; i++) {
+//         if (n % i === 0) result.push(i);
+//     }
     
-    return result;
+//     return result;
+    return Array(n).fill(0).map((v, i) => v+i+1).filter(v => n % v === 0);
 }
